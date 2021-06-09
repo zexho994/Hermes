7b2853408ff5311c05726ea0939ecf8bd7b3a964
zexho <zouzhihao994@gmail.com>
2021-06-09 19:13:31 +0800

    add codec.go

--- codec/codec.go
+++ codec/codec.go
@@ -1,5 +1,18 @@
 package codec
 
+import (
+	"io"
+)
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(closer io.ReadWriteCloser) Codec
+
 type Header struct {
 	// 方法名称
 	serviceMethod string
@@ -7,4 +20,18 @@ type Header struct {
 	seq uint64
 	// 错误信息
 	error string
+}
+
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "application/json"
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
 }
